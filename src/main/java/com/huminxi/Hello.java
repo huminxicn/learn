e95037c46cd9a8c711d735ae5803c719110b0a8b
huminxicn <huminxi2017@gmail.com>
2022-02-09 14:03:20 +0800

    1.调整 .gitignore。

---
+++ src/main/java/com/huminxi/Hello.java
@@ -0,0 +1,13 @@
+package com.huminxi;
+
+/**
+ * 一句话概括功能。
+ *
+ * @author huminxi2017@gmail.com
+ * @since 2022/02/09 13:58
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
