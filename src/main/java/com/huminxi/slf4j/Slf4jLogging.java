82478f05ef91d76a34a80ab582f83d24626cb49c
huminxicn <huminxi2017@gmail.com>
2022-05-21 23:54:01 +0800

    1.增加 logback-test.xml 配置文件。

--- src/main/java/com/huminxi/slf4j/Slf4jLogging.java
+++ src/main/java/com/huminxi/slf4j/Slf4jLogging.java
@@ -1,5 +1,7 @@
 package com.huminxi.slf4j;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +16,14 @@ public class Slf4jLogging {
     private static Logger log = LoggerFactory.getLogger(Slf4jLogging.class);
 
     public static void main(String[] args) {
+        log.trace("hello logging");
+        log.debug("hello logging");
         log.info("hello logging");
+        log.warn("hello logging");
+        log.error("hello logging");
+
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        // print logback's internal status
+        StatusPrinter.print(lc);
     }
 }
